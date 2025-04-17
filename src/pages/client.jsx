4cb2ffef88847630c4628dcfee9367d9974930c7
erza <satriaseptian724@gmail.com>
2025-04-17 10:09:45 +0700

    second

--- src/pages/client.jsx
+++ src/pages/client.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import { supabase } from '../supabaseClient';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
--- src/pages/kontak.jsx
+++ src/pages/kontak.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import {
